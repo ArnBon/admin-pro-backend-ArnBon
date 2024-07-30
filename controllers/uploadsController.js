32a16033b6b759541d765b2402643f1a99c6476f
ArnBon <bonillaarnaldo@gmail.com>
2024-07-30 09:33:46 -0400

    clase 188 optimizacion del email cambiar imagen

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -83,7 +83,7 @@ const fileUpload = (req, res = response) => {
                 res.sendFile(pathImg)                
              } else {
 
-                 const pathImg = path.join(__dirname, `../uploads/no-img.png`);
+                 const pathImg = path.join(__dirname, `../uploads/no-img.jpg`);
                  res.sendFile(pathImg);
              }
         }
